b7d14eea49a6afaec543caf7f7042cdce520d323
mopemope <yutaka.matsubara@gmail.com>
2017-07-14 09:08:25 +0900

    Add android example project

---
+++ android/MyApplication/mylibrary/src/main/java/com/example/mylibrary/Lib.java
@@ -0,0 +1,16 @@
+package com.example.mylibrary;
+
+public class Lib {
+
+  public static void hello() {
+    System.out.println("Hello");
+  }
+
+  public static void hello2() {
+    System.out.println("Hello");
+  }
+
+  public static void hello3() {
+    System.out.println("Hello");
+  }
+}
---
+++ android/MyApplication/mylibrary/src/main/java/com/example/mylibrary/Lib2.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+public class Lib2 {
+
+  public Lib2() {}
+
+  public String greeting() {
+    return "Hello";
+  }
+}
